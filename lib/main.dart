2b1158d24fbf6b3c899197585cc1e2c8cb88dbc2
Maruli <tommypurba73@gmail.com>
2025-08-08 16:28:39 +0700

    add how to manage collor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:thefour/widgets/expense.dart';
 
+var kColourScheem = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
 void main(){
   runApp(
     MaterialApp(
+      theme: ThemeData().copyWith(
+        
+        colorScheme: kColourScheem,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColourScheem.onPrimaryContainer,
+          foregroundColor: kColourScheem.primaryContainer,
+        )
+      ),
       home: Expense(),
     )
   );
